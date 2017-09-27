7d447a1fae5bceeabe33e1a0b7dae84eb8f88dc4
Andrew Belt <andrewpbelt@gmail.com>
2017-09-26 20:49:25 -0400

    Add homepage URL to Plugin metadata

--- src/ESeries.cpp
+++ src/ESeries.cpp
@@ -7,5 +7,6 @@ void init(rack::Plugin *p) {
 	plugin = p;
 	plugin->slug = "ESeries";
 	plugin->name = "E-Series";
+	plugin->homepageUrl = "https://github.com/VCVRack/ESeries";
 	createModel<E340Widget>(plugin, "E340", "E340 Cloud Generator");
 }
