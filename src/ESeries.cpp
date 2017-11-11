ac990ce18327356dd1840b3a83161d17b2e2e183
Andrew Belt <andrewpbelt@gmail.com>
2017-11-11 02:34:13 -0500

    Add tags

--- src/ESeries.cpp
+++ src/ESeries.cpp
@@ -10,5 +10,5 @@ void init(rack::Plugin *p) {
 	p->version = TOSTRING(VERSION);
 #endif
 
-	p->addModel(createModel<E340Widget>("E-Series", "E340", "E340 Cloud Generator"));
+	p->addModel(createModel<E340Widget>("E-Series", "E340", "E340 Cloud Generator", OSCILLATOR_TAG));
 }
